02516f04ab074281162fa63d9c0667820715cf6e
mikhailpastukhov <pastukhov.mi@gmail.com>
2023-07-07 17:29:00 +0200

    cypress fix

--- cypress/support/component.js
+++ cypress/support/component.js
@@ -18,10 +18,17 @@ import './commands'
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
+import {mount} from 'cypress/react18'
+import {Provider} from 'react-redux'
+import {store} from "../../src/store";
+import {MemoryRouter} from 'react-router-dom'
 
-import { mount } from 'cypress/react18'
+Cypress.Commands.add('mount', (component, options = {}) => {
+    // Use the default store if one is not provided
+    const {routerProps = {initialEntries: ['/']}, reduxStore = store, ...mountOptions} = options
 
-Cypress.Commands.add('mount', mount)
-
+    const wrapped = <MemoryRouter {...routerProps}><Provider store={reduxStore}>{component}</Provider></MemoryRouter>
+    return mount(wrapped, mountOptions)
+})
 // Example use:
 // cy.mount(<MyComponent />)
